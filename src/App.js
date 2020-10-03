f070bb79d2b06231430b22eaeaa599278210764e
Carlos Diaz <diazpolanco13@gmail.com>
2020-10-03 19:03:28 -0400

    Primer Commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-gray-700">Hola Mundo!</h1>
+    </div>
+  );
+}
+
+export default App;
