9a3605ab8d29cbee15516e3489a252283d8cfd02
Carlos Diaz <diazpolanco13@gmail.com>
2020-10-03 21:14:50 -0400

    Renombrando componentes

--- src/JournalApp.js
+++ src/JournalApp.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function App() {
+function JournalApp() {
   return (
     <div>
       <h1 className="text-4xl font-bold text-gray-700">Hola Mundo!</h1>
@@ -8,4 +8,4 @@ function App() {
   );
 }
 
-export default App;
+export default JournalApp;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import JournalApp from './JournalApp';
 
 
-ReactDOM.render(
 
-    <App />, 
-  document.getElementById('root')
-);
+ReactDOM.render(<JournalApp />, document.getElementById("root"));
