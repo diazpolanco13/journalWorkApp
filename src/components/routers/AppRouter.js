923f9c5d69fd673e745491c69cb54160fcd8ef7a
Carlos Diaz <diazpolanco13@gmail.com>
2020-10-04 13:56:28 -0400

    configurando postPurge

--- src/components/routers/AppRouter.js
+++ src/components/routers/AppRouter.js
@@ -1,23 +1,17 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Redirect, Route } from "react-router-dom";
 import { JournalScreen } from "../journal/JournalScreen";
 import { AuthRouter } from "./AuthRouter";
 
 
 export const AppRouter = () => {
     return (
-        <Router>
-            <div>
-                <Route
-                    path="/auth"
-                      component={AuthRouter} 
-                />
-                <Route
-                    exact
-                    path="/"
-                    component={JournalScreen} 
-                />
-            </div>
-        </Router>
-    )
+      <Router>
+        <div>
+          <Route path="/auth" component={AuthRouter} />
+          <Route exact path="/" component={JournalScreen} />
+          <Redirect to="/auth/login" />
+        </div>
+      </Router>
+    );
 }
